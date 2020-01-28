6b37fffa91c650154d799bc141200e71b2cf97ad
Lucas Taniguti <lucas.taniguti@mendelics.com.br>
2020-01-28 09:50:03 -0300

    dist and better readme

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='iap-auth',
-    version='0.0.1',
+    version='0.0.2',
     description='Perform authentication for Google Cloud Identity Aware Proxy from a service account',
-    url='git@github.com:lmtani/iap-auth.git',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/lmtani/iap-auth',
     author='Lucas Taniguti',
     author_email='ltaniguti@gmail.com',
     license='unlicense',
